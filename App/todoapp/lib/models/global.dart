d88face918957aa5133218ecbac05aadab2d8dca
Jaidyn Belbin <jaidyn.belbin797@gmail.com>
2021-03-21 21:55:46 +0800

    Created top nav bar and intray title with FAB

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Global styles for our app.
+Color darkGreyColour = Color(0xff212128);
+Color redColour = Color(0xffdc4f64);
+TextStyle intrayTitleStyle = TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColour,
+    fontSize: 40);
